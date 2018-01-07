6c1908371635769c69a1cf422b82a23abed0b7a1
Daniel Roschka <danielroschka@phoenitydawn.de>
2018-01-07 12:35:14 +0100

    Update copyright year

--- xpartamupp/lobby_ranking.py
+++ xpartamupp/lobby_ranking.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2017 Wildfire Games.
+# Copyright (C) 2018 Wildfire Games.
 # This file is part of 0 A.D.
 #
 # 0 A.D. is free software: you can redistribute it and/or modify
